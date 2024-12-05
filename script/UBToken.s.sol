60a92b317d0c1374c9dec78cd2774dbe588a76e8
Louis-VI-XVIII <pierrelouistidou@rocketmail.com>
2024-12-04 21:53:47 -0500

    3

--- script/UBToken.s.sol
+++ script/UBToken.s.sol
@@ -1,19 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import {UBToken} from "../src/UBToken.sol";
-import {Script} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script";
 
 contract UBTokenDeploy is Script {
     function run() public {
-        
         uint256 initialSupply = 1_000_000 * 10 ** 18;
         vm.startBroadcast();
 
-    
         UBToken ubToken = new UBToken(initialSupply);
 
-        
         ubToken.transfer(0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc, 100 * 10 ** 18);
         vm.stopBroadcast();
     }
